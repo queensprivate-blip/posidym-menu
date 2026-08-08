286433841219b7d6511f0f454f190ad67a4f82bd
queensprivate-blip <queensprivate@gmail.com>
2026-08-08 16:30:37 +0300

    Add files via upload

---
+++ supabase/update-review-url-v24.sql
@@ -0,0 +1,3 @@
+update public.venue_settings
+set review_url = 'https://yandex.ru/maps/213/moscow/?ll=37.698523%2C55.750618&mode=poi&poi%5Bpoint%5D=37.697811%2C55.751194&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D245377761010&tab=reviews&z=18.12'
+where id = 1;
